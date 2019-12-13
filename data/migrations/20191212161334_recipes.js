8899193ba0f083c603d16108c49fcaf2ca46d578
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2019-12-12 19:10:58 -0500

    migrations file setup

--- data/migrations/20191212161334_recipes.js
+++ data/migrations/20191212161334_recipes.js
@@ -1,25 +1,52 @@
 exports.up = function(knex) {
-	return knex.scheme
+	return knex.schema
 		.createTable('recipes', (tbl) => {
 			tbl.increments();
 
-			tbl.string('name', 255).notNullAble();
+			tbl.string('name', 255).notNullable();
 		})
 		.createTable('ingredients', (tbl) => {
 			tbl.increments();
 
-			tbl.string('name', 255).notNullAble();
+			tbl.string('name', 255).notNullable();
 		})
 		.createTable('instructions', (tbl) => {
 			tbl.increments();
-			tbl.string('instructions', 255).notNullAble();
-			tbl.integer('steps_num').notNullAble();
-			tbl.integer('recipe_id').notNullAble();
+			tbl.string('instructions', 255).notNullable();
+			tbl.integer('steps_num').notNullable();
+			// tbl.integer('recipes_id').notNullable();
+
+			tbl
+				.integer('recipes_id')
+				.unsigned()
+				.notNullable()
+				.references('id')
+				.inTable('recipes')
+				.onDelete('RESTRICT')
+				.onUpdate('CASCADE'); //FORIEGN KEY for recipe id to recipes table
 		})
 		.createTable('recipe_ingred', (tbl) => {
-			tbl.increments('recipe_id');
-			tbl.integer('ingred_id').notNullAble();
-			tbl.float('quantity').notNullAble();
+			tbl.increments();
+			// tbl.integer('ingred_id').notNullable();
+			tbl.float('quantity').notNullable();
+
+			tbl
+				.integer('recipe_id')
+				.unsigned()
+				.notNullable()
+				.references('id')
+				.inTable('recipes')
+				.onDelete('RESTRICT')
+				.onUpdate('CASCADE'); //FORIEGN KET for recipe id to recipe table
+
+			tbl
+				.integer('ingred_id')
+				.unsigned()
+				.notNullable()
+				.references('id')
+				.inTable('ingredients')
+				.onDelete('RESTRICT')
+				.onUpdate('CASCADE');
 		});
 };
 
